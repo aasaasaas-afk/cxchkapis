532501ed463e895e30fefb11d37ba386b97fae26
aasaasaas-afk <aadiimoga2009@gmail.com>
2025-11-16 19:13:05 +0530

    Update app.py

--- Rocky/app.py
+++ Rocky/app.py
@@ -258,4 +258,5 @@ def index():
     return render_template_string(html_content)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
+
